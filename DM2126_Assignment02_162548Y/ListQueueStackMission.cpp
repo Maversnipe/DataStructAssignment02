a0e10bfc44bd52fd592a3ff859a4b4e7b7da1e5c
DESKTOP-NFT7BUK\user <lucas.nguyen123@gmail.com>
2017-01-31 11:32:07 +0800

    1. Added comments to push_back() function in linked list 2. Added comments to pop_front() function in linked list 3. Added codes to insert_at() function in linked list 4. Added codes to pop_at() function in linked list 5. Added codes to size() function in linked list

--- DM2126_Assignment02_162548Y/ListQueueStackMission.cpp
+++ DM2126_Assignment02_162548Y/ListQueueStackMission.cpp
@@ -58,32 +58,32 @@ void LinkedList::push_front(int data)
 
 void LinkedList::push_back(int data)
 {
-	Node newNode(data);
-	Node* curr = head_;
-	if (size() == 0)
-		head_ = &newNode;
+	Node newNode(data);			// Create a new node with the new data inside
+	Node* curr = head_;			// Create a new node pointer, curr, which points to where head_ is pointing
+	if (size() == 0)			
+		head_ = &newNode;		// If size() is 0, head_ pointer will just point to new node
 	else
-	{
-		while (curr->next != NULL)
+	{ // If size() is not 0
+		while (curr->next != NULL) // Loops to end of linked list
 			curr = curr->next;
-		curr->next = &newNode;
+		curr->next = &newNode;	// Points the last node's next pointer to the new node
 	}
 }
 
 int LinkedList::pop_front()
 {
-	int deletedVal = 0;
+	int deletedVal = 0;			// Create an int to store the value of the deleted node
 
-	if (size() == 0)
-		return deletedVal;
+	if (size() == 0)	
+		return deletedVal;		// Returns 0 if size() is 0
 
-	Node* curr = head_;
-	head_ = head_->next;
-	deletedVal = curr->data;
-	curr = NULL;
-	delete curr;
+	Node* curr = head_;			// Create a node pointer, curr, which points to where head_ is pointing
+	head_ = head_->next;		// Points head pointer to the next node
+	deletedVal = curr->data;	// Put the value of the node to be popped into deletedVal
+	curr = NULL;				// Make curr pointer become NULL
+	delete curr;				// Delete the curr Pointer
 
-    return deletedVal;
+    return deletedVal;			// Returns the popped node's value
 }
 
 int LinkedList::pop_back()
@@ -110,17 +110,74 @@ int LinkedList::pop_back()
 }
 
 void LinkedList::insert_at(int pos, int data)
-{    
+{ 
+	if (pos <= 0)
+	{
+		push_front(data);
+	}
+	else if (pos > (size() - 1))
+	{
+		push_back(data);
+	}
+	else if (pos < size())
+	{
+		Node* beforeCurr = head_;
+		Node* curr = head_->next;
+		Node newNode(data);
+
+		for (int placeToBeInserted = 1; placeToBeInserted < pos; placeToBeInserted++)
+		{
+			beforeCurr = curr;
+			curr = curr->next;
+		}
+
+		beforeCurr->next = &newNode;
+		newNode.next = curr;
+	}
 }
 
 int LinkedList::pop_at(int pos)
 {
-    return 0;
+	if (pos <= 0)
+	{
+		return pop_front();
+	}
+	else if (pos > (size() - 1))
+	{
+		return pop_back();
+	}
+	else if (pos < size())
+	{
+		int deletedVal = 0;
+		Node* beforeCurr = head_;
+		Node* curr = head_->next;
+
+		for (int placeToBeInserted = 1; placeToBeInserted < pos; placeToBeInserted++)
+		{
+			beforeCurr = curr;
+			curr = curr->next;
+		}
+
+		deletedVal = curr->data;
+		curr = NULL;
+		beforeCurr->next = NULL;
+		delete beforeCurr->next;
+		delete curr;
+
+		return deletedVal;
+	}
 }
 
 size_t LinkedList::size()
 {
-    return 0;
+	size_t theSize = 0;
+	Node* curr = head_;
+	while (curr->next)
+	{
+		theSize++;
+		curr = curr->next;
+	}
+    return theSize;
 }
 
 //*******************************************************************//
